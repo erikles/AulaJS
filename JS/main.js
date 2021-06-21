bcb6600ccc4c783baa1583f1127bdd80abb990f5
erikles <57198881+erikles@users.noreply.github.com>
2021-06-21 15:38:56 -0300

    Alteração

---
+++ JS/main.js
@@ -0,0 +1,96 @@
+function clicou(){
+    document.getElementById("agradecimento").innerHTML ="<b>Obrigadoo por clicar</b>";
+
+    
+    //alert("Obg por clicar")
+}
+
+function redirecionar(){
+    window.open("https://google.com")
+    //window.location.href = "https://google.com"
+}
+
+function trocar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Obrigado por passar o mouse"
+    //alert("Trocar texto")
+    elemento.innerHTML = "Obg por passar o mouse"
+}
+
+function voltar(elemento){
+    //document.getElementById("mousemove").innerHTML = "Passe o mouse aqui"
+    elemento.innerHTML = "Passe o mouse aqui"
+
+}
+
+function load(){
+    alert("Página carregada")
+}
+
+/*function soma(n1,n2){
+    return n1 + n2;
+}
+*/
+
+/*
+function validaIdade(idade){
+    var validar;
+    if (idade >= 18){
+        validar =true
+    }else{
+        validar = false
+    }
+    return validar;
+    
+}
+
+var idade = prompt("Qual sua idade?");
+console.log(validaIdade(idade));
+*/
+//alert(soma(5,10));
+
+//var d = new Date();
+//alert(d.getMonth()+1);
+
+/*var count;
+for(count = 0; count <=5; count++){
+    alert(count);
+}
+*/
+/*var count = 0;
+while (count <= 5){
+    console.log(count);
+    count++;
+}
+*/
+
+//var nome = "Erikles José";
+//var idade = 25;
+//var idade2 = 5;
+//var frase = "Japão é o mehor time do mundo";
+
+//alert(nome + " tem " + idade + " anos");
+
+//console.log(nome);
+//console.log(idade+idade2);
+//console.log(frase.toUpperCase());
+
+//var lista = ["maça", "pera", "laranja"];
+//lista.push("uva");
+//lista.pop;
+//console.log(lista.reverse());
+//console.log(lista.toString());
+//console.log(lista.join(" | "));
+
+/*var frutas = [{nome: "Maça", cor: "vermelha"}, {nome:"Uva", cor:"Roxo"}]
+console.log(frutas[0])
+*/
+/*
+var idade = prompt("Qual sua idade?")
+if (idade >= 18){
+    alert("Maior de idade");
+}
+else{
+    alert("Menor de idade");
+}
+*/
+
